af3839f5164821a63991e58e5335fc99d05d15e9
Pierre-Alexandre St-Jean <pastjean@coveo.com>
2016-04-29 09:44:45 -0400

    🎨

--- repo.go
+++ repo.go
@@ -44,27 +44,8 @@ func main() {
 		log.Fatalf("Error: \"Could not update\" %s", err)
 	}
 
+	// TODO: make pull request
 	// }
-}
-
-func hgClone(source, to string) error {
-	return execute("hg", "clone", source, to)
-}
-
-func hgUpdate(repository, rev string) error {
-	return execute(repository, "hg", "update", rev)
-}
-
-func hgBranch(repository, branchname string) error {
-	return execute(repository, "hg", "branch", branchname)
-}
-
-func hgCommit(repository, message string) error {
-	return execute(repository, "hg", "commit", message)
-}
-
-func hgPush(repository, remote string) error {
-	return execute(repository, "hg", "push", remote)
 }
 
 func execute(pwd string, command string, params ...string) error {

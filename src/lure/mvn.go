69fa3235ff9165c51709a57fd8e96d305e05efbc
David Reisch <dreisch@MacBook-Pro-3.local>
2016-09-29 16:58:57 -0400

    Added missing file.

---
+++ src/lure/mvn.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"log"
+	"os/exec"
+	"regexp"
+
+	"github.com/k0kubun/pp"
+	"fmt"
+)
+
+func mvnOutdated(path string) []moduleVersion {
+	cmd := exec.Command("mvn", "versions:display-dependency-updates")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	cmd.Dir = path
+	err := cmd.Run()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	reader := bytes.NewReader(out.Bytes())
+	scanner := bufio.NewScanner(reader)
+
+	//mvnRegex, _ := regexp.Compile(`\s*\[INFO\]\s+((?:[a-zA-Z0-9$_-]\.?))+:((?:[a-zA-Z0-9$_-]\.?))\s+\.{34}\s+((?:[a-zA-Z0-9$_-]\.?))\s+->\s+((?:[a-zA-Z0-9$_-]\.?))\s*`)
+	mvnRegex, _ := regexp.Compile(`\s*\[INFO\]\s+((?:[a-zA-Z0-9$_-]\.?)+):((?:[a-zA-Z0-9$_-]\.?)+)\s+\.+\s+((?:[a-zA-Z0-9$_-]\.?)+)\s+->\s+((?:[a-zA-Z0-9$_-]\.?)+)\s*`)
+
+	version := make([]moduleVersion, 0, 0)
+	for scanner.Scan() {
+		result := mvnRegex.FindStringSubmatch(scanner.Text())
+		fmt.Printf("> %s - %q\n", scanner.Text(), result)
+		if result != nil {
+			mv := moduleVersion{
+				Module:  result[1] + result[2],
+				Current: result[3],
+				Latest:  result[4],
+			}
+
+			pp.Println(mv)
+			version = append(version, mv)
+		}
+	}
+
+	return version
+}

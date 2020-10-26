ba7fc2474cd12518cabe76e2f8b3fe09d1791027
Simon Toussaint <stoussaint@coveo.com>
2020-10-26 14:34:06 -0400

    Adding json mapping to PullRequest type

--- lib/lure/repositorymanagementsystem/pullRequest.go
+++ lib/lure/repositorymanagementsystem/pullRequest.go
@@ -5,12 +5,12 @@ type Branch interface {
 }
 
 type PullRequest struct {
-	ID                int
-	Title             string
-	Description       string
-	Source            Branch
-	Dest              Branch
-	CloseSourceBranch bool
-	State             string
-	Reviewers         []user
+	ID                int    `json:"id"`
+	Title             string `json:"title"`
+	Description       string `json:"description"`
+	Source            Branch `json:"source"`
+	Dest              Branch `json:"dest"`
+	CloseSourceBranch bool   `json:"close_source_branch"`
+	State             string `json:"state"`
+	Reviewers         []user `json:"reviewers"`
 }
